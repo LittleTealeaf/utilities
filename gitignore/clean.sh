4c8bf5c73770696e8f0cc0db223b0007a2461836
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-06 10:32:16 -0500

    quick change

--- gitignore/clean.sh
+++ gitignore/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Simple script to clean out all latex files
+# Simple script to clean out all files listed in .gitignore
 cat .gitignore | grep -v "#" | while read line; do
 	find . -name "$line" -type f -not -path "./.git/*" | while read file; do
 		echo "Deleting $file"
